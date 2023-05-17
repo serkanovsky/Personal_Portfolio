87a76e09e769ac96b2bf25db07c187e242e140f2
Serkan Kılıçkap <serkankilickap@gmail.com>
2023-05-17 21:12:34 +0300

    Add project files.

---
+++ DataAccessLayer/EntityFramework/EfPortfolioDal.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Abstact;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfPortfolioDal:GenericRepository<Portfolio>,IPortfolioDal
+    {
+    }
+}
