d768b7fb0646675a790d6ce6817d0f2070d53fb2
Serkan Kılıçkap <serkankilickap@gmail.com>
2023-05-25 21:29:31 +0300

    TestimonialList completed Experience Completed

--- EntityLayer/Concrete/Testimonial.cs
+++ EntityLayer/Concrete/Testimonial.cs
@@ -13,5 +13,6 @@ namespace EntityLayer.Concrate
         public string Company { get; set; }
         public string Comment { get; set; }
         public string ImageUrl { get; set; }
+        public string Title { get; set; }
     }
 }
