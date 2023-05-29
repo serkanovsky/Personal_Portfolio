f6f745a3e8001f3884d2f1926878c8df5808a969
Serkan Kılıçkap <serkankilickap@gmail.com>
2023-05-29 22:45:18 +0300

    Partial SideBar Completed

---
+++ PersonalPortfolio/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalPortfolio.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
