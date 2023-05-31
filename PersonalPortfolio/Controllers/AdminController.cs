09aa03c2475c01f1ebe5a64bc867bda347c590d6
Serkan Kılıçkap <serkankilickap@gmail.com>
2023-05-31 23:32:24 +0300

    Admin Layout Partial Configuration

--- PersonalPortfolio/Controllers/AdminController.cs
+++ PersonalPortfolio/Controllers/AdminController.cs
@@ -8,5 +8,22 @@ namespace PersonalPortfolio.Controllers
         {
             return PartialView();
         }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+
     }
 }
